af2686a76f5daf4a8a28786dec55db504dfdf0b6
k <401208163@qq.com>
2019-11-15 16:57:16 +0800

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { IntroductionComponent } from './Introduction/Introduction.component';
+import { PageComponent } from './page/page.component';
+import { TodomvcComponent } from './todomvc/todomvc.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/layout',
+    pathMatch:  'full'
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: IntroductionComponent
+      },
+      {
+        path: 'page',
+        component: PageComponent
+      },
+      {
+        path: 'todomvc',
+        component: TodomvcComponent
+      }
+    ]
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
